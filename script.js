a33e4603faa19ee9f575183b52017871ce75e776
sadeeshan <sadeeshanlk3@gmail.com>
2026-04-08 12:52:54 +0530

    Update js code

---
+++ script.js
@@ -0,0 +1,52 @@
+window.onload = () => {
+
+    if(document.querySelector(".typing-text")) {
+        new Typed(".typing-text", {
+            strings: ["UI/UX Designer", "Software Engineer", "Web Developer"],
+            typeSpeed: 100,
+            backSpeed: 60,
+            loop: true
+        });
+    }
+
+
+    let sections = document.querySelectorAll('section');
+    let navLinks = document.querySelectorAll('.header .navbar a');
+
+    window.onscroll = () => {
+        sections.forEach(sec => {
+            let top = window.scrollY;
+            let offset = sec.offsetTop - 150;
+            let height = sec.offsetHeight;
+            let id = sec.getAttribute('id');
+
+            if(top >= offset && top < offset + height) {
+                navLinks.forEach(links => {
+                    links.classList.remove('active');
+                    let targetLink = document.querySelector('.header .navbar a[href*=' + id + ']');
+                    if(targetLink) targetLink.classList.add('active');
+                });
+            }
+        });
+
+
+        reveal();
+    };
+
+
+    function reveal() {
+        var reveals = document.querySelectorAll(".reveal");
+        reveals.forEach(el => {
+            var windowHeight = window.innerHeight;
+            var elementTop = el.getBoundingClientRect().top;
+            var elementVisible = 150;
+            if (elementTop < windowHeight - elementVisible) {
+                el.classList.add("active");
+            }
+        });
+    }
+
+
+
+    reveal();
+};
